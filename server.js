bfac25b93e390401709a8d0a5f5e93f682819583
arapsingh <buip930@gmail.com>
2024-10-08 11:18:26 +0700

    add token

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const host = '0.0.0.0';
 const port = process.env.PORT || 8080; // Render tự động thiết lập biến PORT
 
 corsAnywhere.createServer({
-    requireHeader: ['origin', 'x-requested-with'], // Yêu cầu header
-    removeHeaders: ['cookie', 'authorization'], // Loại bỏ cookie và authorization nếu cần
+    requireHeader: [ 'x-requested-with'], // Yêu cầu header
+    removeHeaders: ['cookie'], // Loại bỏ cookie và authorization nếu cần
 }).listen(port, host, () => {
     console.log(`CORS Anywhere server is running on ${host}:${port}`);
 });
