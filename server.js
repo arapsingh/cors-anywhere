051648aa6f4036089861a2966298bc2f018490b6
arapsingh <buip930@gmail.com>
2024-10-08 10:26:21 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const corsAnywhere = require('cors-anywhere');
+
+const host = '0.0.0.0';
+const port = process.env.PORT || 8080; // Render tự động thiết lập biến PORT
+
+corsAnywhere.createServer({
+    requireHeader: ['origin', 'x-requested-with'], // Yêu cầu header
+    removeHeaders: ['cookie', 'authorization'], // Loại bỏ cookie và authorization nếu cần
+}).listen(port, host, () => {
+    console.log(`CORS Anywhere server is running on ${host}:${port}`);
+});
